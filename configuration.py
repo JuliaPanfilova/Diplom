9d6e000f5278172a20d84619e3e59fd1055fcc42
JuliaPanfilova <pdv-78@mail.ru>
2023-09-27 18:53:55 +0300

    Diplomnew

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://d9b3876f-edbc-4a23-a017-3e94905cee42.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER_TRACK = "/api/v1/orders/track?t="
---
+++ order_test.py
@@ -0,0 +1,17 @@
+# Юлия Панфилова, 8а когорта - финальный проект. Инженер по тестированию плюс.
+
+import data
+import sender_stand_request
+
+def test_order_create():
+    response_new_order = sender_stand_request.post_new_order(data.order_body)
+
+    track_number = response_new_order.json().get("track")
+    print(track_number)
+
+    response_number = sender_stand_request.get_order(track_number)
+
+    assert response_number.status_code == 200
+    order_data = response_number.json()
+    print(order_data)
+
