5fd0dcfe3f5667ccd2e2d51d61500a9e2983c99a
JuliaPanfilova <146200589+JuliaPanfilova@users.noreply.github.com>
2023-09-28 21:25:33 +0300

    Add files via upload

---
+++ sender_stand_request.py
@@ -0,0 +1,18 @@
+import requests
+import configuration
+import data
+
+def post_new_order(order_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
+        json=order_body)
+
+response_new_order = post_new_order(data.order_body)
+print(response_new_order.status_code)
+print(response_new_order.json())
+track_number = (response_new_order.json()["track"])
+
+def get_order(track_number):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_TRACK + str(track_number),
+                        params={"track":track_number})
+response_order_track = get_order(track_number);
+print(response_order_track.json())
